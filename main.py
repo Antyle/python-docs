976922af78e1a932300794b31179227e63cd690f
Antyle <152796256+Antyle@users.noreply.github.com>
2026-05-27 15:14:11 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# this is a file for listing every things i know in python (i think so) since i'm a junior dev i'm still learning 
+
+# 1. variable
+
+str = "10.0"
+float = 10.0
+int = 10
+list = [1, 2, 3]
+# 2. while & for
+
+variables = [1, 2, 3]
+for variable in variables:
+    variable += 1
+    print(variable)
+
+for variable in range(10):
+    variable += 1
+    print(variable)
+
+word = "J'aime l'argent"
+
+for letter in word:
+    print(letter)
+
+while i > 1:
+    i += 1
+    
+# 
+    
+
+
